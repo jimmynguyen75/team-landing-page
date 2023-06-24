57032f26ee419021b4ad674e079f676e0e4e6677
Jimmy <minhthusdb@gmail.com>
2023-06-24 20:49:25 +0700

    last deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {experimental:{appDir: true}}
+const nextConfig = {
+  experimental: { appDir: true },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
