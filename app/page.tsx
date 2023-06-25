6f3a9c5cf1f5e3eae6ee599c8089819399a8e728
Jimmy <minhthusdb@gmail.com>
2023-06-25 20:33:47 +0700

    last change

--- app/page.tsx
+++ app/page.tsx
@@ -14,37 +14,75 @@ import { fetchProjects } from "@/utils/fetchProjects";
 import { fetchSkills } from "@/utils/fetchSkills";
 import { fetchSocials } from "@/utils/fetchSocials";
 
-type Props = {
-  pageInfo?: PageInfo;
-  experiences?: Experience[];
-  skills?: Skill[];
-  projects?: Project[];
-  socials?: Social[];
-};
+// type Props = {
+//   socials?: Social[];
+// }
+// const fetchSocial = async () => {
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/socials`, { next: { revalidate: 60 } });
+//   const socials: Social[] = await res.json();
+//   console.log("noooo");
+//   return socials;
+// };
 
-export default async function () {
-  const socials: Social[] = []//await fetchSocials();
+async function getSocial() {
+  const socials: Social[] = await fetchSocials();
+  if (!socials) {
+    throw new Error("Failed to fetch data");
+  }
+  return socials;
+}
+async function getPageInfo() {
   const pageInfo: PageInfo = await fetchPageInfo();
-  const experiences: Experience[] = []//await fetchExperience();
-  const skills: Skill[] = []//await fetchSkills();
-  const projects: Project[] = []//await fetchProjects();
+  if (!pageInfo) {
+    throw new Error("Failed to fetch data");
+  }
+  return pageInfo;
+}
+async function getExperience() {
+  const experiences: Experience[] = await fetchExperience();
+  if (!experiences) {
+    throw new Error("Failed to fetch data");
+  }
+  return experiences;
+}
+async function getSkill() {
+  const skills: Skill[] = await fetchSkills();
+  if (!skills) {
+    throw new Error("Failed to fetch data");
+  }
+  return skills;
+}
+async function getProject() {
+  const projects: Project[] = await fetchProjects();
+  if (!projects) {
+    throw new Error("Failed to fetch data");
+  }
+  return projects;
+}
+const Home = async () => {
+  const pageInfo: PageInfo = await getPageInfo();
+  const experiences: Experience[] = await getExperience();
+  const skills: Skill[] = await getSkill();
+  const projects: Project[] = await getProject();
+  const socials: Social[] = await getSocial();
+
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar-thin scrollbar-thumb-[#F7AB0A]/80 scrollbar-track-gray-400/20">
-      <Header socials={socials}/>
+      <Header socials={socials} />
       <section id="hero" className="snap-start">
-        <Hero pageInfo={pageInfo}/>
+        <Hero pageInfo={pageInfo} />
       </section>
       <section id="about" className="snap-center">
-        <About pageInfo={pageInfo}/>
+        <About pageInfo={pageInfo} />
       </section>
       <section id="experience" className="snap-center">
-        <WorkExperience experiences={experiences}/>
+        <WorkExperience experiences={experiences} />
       </section>
       <section id="skills" className="snap-start">
-        <Skills skills={skills}/>
+        <Skills skills={skills} />
       </section>
       <section id="projects" className="snap-start">
-        <Projects projects={projects}/>
+        <Projects projects={projects} />
       </section>
       <section id="contact" className="snap-start">
         <ContactMe />
@@ -63,7 +101,7 @@ export default async function () {
     </div>
   );
 };
-
+export default Home;
 // export const getStaticProps: GetStaticProps = async () => {
 //   console.log("getStaticProps()");
 //   const pageInfo: PageInfo = await fetchPageInfo();
