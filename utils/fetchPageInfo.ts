e0db0991ea3dca7aa9c3863432cd5e0c1f45cbd0
Jimmy <minhthusdb@gmail.com>
2023-06-25 14:43:09 +0700

    okkk

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -1,7 +1,7 @@
 import { PageInfo } from "@/typings";
 
 export const fetchPageInfo = async() => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/pageInfo`, { next: { revalidate: 10 } });
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/pageInfo`);
 
     const data = await res.json();
     const pageInfo: PageInfo = data.pageInfo;
